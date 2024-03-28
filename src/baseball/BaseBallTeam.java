01d1573ee4a4fbe97699dfd1bbcb0f71bb4ae61e
sango366 <txobitxobi.1@gmail.com>
2024-03-29 00:02:03 +0900

    課題kadai-java-review03の提出

---
+++ src/baseball/BaseBallTeam.java
@@ -0,0 +1,31 @@
+package baseball;
+
+public class BaseBallTeam {
+    private String name;
+    private int win;
+    private int lose;
+    private int draw;
+    double getrate;
+
+    //
+    public BaseBallTeam(String name,int wins, int loss, int draw) {
+        this.name = name;
+        this.win = wins;
+        this.lose = loss;
+        this.draw = draw;
+        getrate = getRate();
+        report();
+
+    }
+
+    public double getRate() {
+        return (double) this.win / (this.win + this.lose);
+
+    }
+
+    public void report() {
+        System.out.println(name + "の2022年の成績は" + win + "勝" + lose + "敗" + draw + "分、勝率は" + getrate + "です。");
+    }
+
+}
+
