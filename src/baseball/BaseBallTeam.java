e2beebbb7a42ccbe13f600eebf272b63b489c734
sango366 <txobitxobi.1@gmail.com>
2024-03-30 22:08:41 +0900

    フィールドからgetrateの削除 reportメソッドからgetRateを呼び出すように改修 Review03からreportメソッドを呼び出すように改修

--- src/baseball/BaseBallTeam.java
+++ src/baseball/BaseBallTeam.java
@@ -5,31 +5,25 @@ public class BaseBallTeam {
     private int win;
     private int lose;
     private int draw;
-    double getrate;
 
-    //エラー回避のためコントラクタ引数なしも作成する
+    // エラー回避のためコントラクタ引数なしも作成する
     public BaseBallTeam() {
     }
 
-    //コントラクタ引数アリを設定する
-    public BaseBallTeam(String name,int wins, int loss, int draw) {
+    // コントラクタ引数アリを設定する
+    public BaseBallTeam(String name, int wins, int loss, int draw) {
         this.name = name;
         this.win = wins;
         this.lose = loss;
         this.draw = draw;
-        getrate = getRate();
-        report();
-
     }
 
     public double getRate() {
         return (double) this.win / (this.win + this.lose);
-
     }
 
     public void report() {
-        System.out.println(name + "の2022年の成績は" + win + "勝" + lose + "敗" + draw + "分、勝率は" + getrate + "です。");
+        double rate = getRate(); // getRateメソッドを呼び出して勝率を求める
+        System.out.println(name + "の2022年の成績は" + win + "勝" + lose + "敗" + draw + "分、勝率は" + rate + "です。");
     }
-
 }
-
--- src/baseball/Review03.java
+++ src/baseball/Review03.java
@@ -4,11 +4,19 @@ public class Review03 {
 
     public static void main(String[] args) {
         BaseBallTeam tokyo = new BaseBallTeam("東京ヤクルトスワローズ" ,80,59,4);
+        tokyo.report();
         BaseBallTeam yokohama = new BaseBallTeam("横浜DeNAベイスターズ", 73, 68, 2);
+        yokohama.report();
         BaseBallTeam hanshin = new BaseBallTeam("阪神タイガース", 68, 71, 4);
+        hanshin.report();
         BaseBallTeam yomiuri = new BaseBallTeam("読売ジャイアンツ", 68, 72, 3);
+        yomiuri.report();
         BaseBallTeam hiroshima = new BaseBallTeam("広島東洋カープ", 66, 74, 3);
+        hiroshima.report();
         BaseBallTeam tyuunichi = new BaseBallTeam("中日ドラゴンズ", 66, 75, 2);
+        tyuunichi.report();
+
+
     }
 
 }
